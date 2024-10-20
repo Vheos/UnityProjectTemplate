6f206211e71c1222ef078c70787c29b40e544d70
Vheos <Vheos777@gmail.com>
2024-10-20 16:23:56 +0200

    Updated global usings

--- Assets/Code/Editor/GlobalUsings.cs
+++ Assets/Code/Editor/GlobalUsings.cs
@@ -1,9 +1,16 @@
+// C#
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using Object = System.Object;
+global using Random = System.Random;
 
+// Unity
 global using UnityEngine;
 global using UnityEngine.Events;
-global using UnityEditor;
+global using UnityObject = UnityEngine.Object;
+global using UnityRandom = UnityEngine.Random;
 
-global using UnityProject;
+// Unity Editor
+global using UnityEditor;
+global using UnityEditorInternal;
--- Assets/Code/GlobalUsings.cs
+++ Assets/Code/GlobalUsings.cs
@@ -1,6 +1,12 @@
+// C#
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using Object = System.Object;
+global using Random = System.Random;
 
+// Unity
 global using UnityEngine;
 global using UnityEngine.Events;
+global using UnityObject = UnityEngine.Object;
+global using UnityRandom = UnityEngine.Random;
