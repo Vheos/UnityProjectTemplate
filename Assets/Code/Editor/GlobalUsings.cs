5bbf561176e5aaed6277d63fb1e287447c00d8ee
Vheos <Vheos777@gmail.com>
2024-10-20 15:24:41 +0200

    Added global usings

---
+++ Assets/Code/Editor/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+
+global using UnityEngine;
+global using UnityEngine.Events;
+global using UnityEditor;
+
+global using UnityProject;
