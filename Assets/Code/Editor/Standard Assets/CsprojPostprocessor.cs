039617ef21a24dde35ce3bffe6f37fd9361a651a
Vheos <Vheos777@gmail.com>
2024-10-21 22:36:25 +0200

    Replaced `Directory.Build.props` (source in desc)
    https://gist.github.com/KanaHayama/93e356bd92b2fc963426bd2d7bd356f8

---
+++ Assets/Code/Editor/Standard Assets/CsprojPostprocessor.cs
@@ -0,0 +1,39 @@
+namespace UnityProject.Editor;
+using System.Text.RegularExpressions;
+
+public class CsprojPostprocessor : AssetPostprocessor
+{
+	// Keys
+	private const string LangVersionKey = "LangVersion";
+	private const string NullableKey = "Nullable";
+	private const string TargetFrameworkVersionKey = "TargetFrameworkVersion";
+	private const string TargetFrameworkKey = "TargetFramework";
+
+	// Settings
+	private const string LangVersion = "11.0";
+	private const string TargetFramework = "netstandard2.1";
+	private const bool EnableNullableContext = true;
+
+	// Methods
+	private static string OnGeneratedCSProject(string _, string content)
+	{
+		string pattern, replacement;
+
+		// LangVersion, Nullable
+		pattern = CreateKeyValue(LangVersionKey, ".*?");
+		replacement = CreateKeyValue(LangVersionKey, LangVersion);
+		if (EnableNullableContext)
+			replacement += '\n' + CreateKeyValue(NullableKey, "enable");
+
+		content = Regex.Replace(content, pattern, replacement);
+
+		// TargetFramework
+		pattern = CreateKeyValue(TargetFrameworkVersionKey, ".*?");
+		replacement = CreateKeyValue(TargetFrameworkKey, TargetFramework);
+		content = Regex.Replace(content, pattern, replacement);
+
+		return content;
+	}
+	private static string CreateKeyValue(string key, string value)
+		=> $"<{key}>{value}</{key}>";
+}
