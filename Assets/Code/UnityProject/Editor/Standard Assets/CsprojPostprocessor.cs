bd2ebf2f24cdb08116d7eab2baed1951549f4876
Vheos <Vheos777@gmail.com>
2025-07-22 02:35:41 +0200

    Reworked `CsprojPostprocessor`

--- Assets/Code/UnityProject/Editor/Standard Assets/CsprojPostprocessor.cs
+++ Assets/Code/UnityProject/Editor/Standard Assets/CsprojPostprocessor.cs
@@ -1,56 +1,70 @@
 #pragma warning disable
 
+using System.IO;
 using System.Text.RegularExpressions;
-using UnityEditor;
 
-namespace UnityProject.Editor
+public class CsprojPostprocessor : AssetPostprocessor
 {
-	public class CsprojPostprocessor : AssetPostprocessor
+	// Settings
+	private const bool ForceCSharp11 = true;
+	private const bool ForceNullable = true;
+	private const bool ForceNetStandard21 = true;
+	private static readonly HashSet<string> Whitelist = new()
 	{
-		// Const
-		private const string LangVersion = "LangVersion";
-		private const string Nullable = "Nullable";
-		private const string TargetFrameworkVersion = "TargetFrameworkVersion";
-		private const string TargetFramework = "TargetFramework";
-		private const string PropertyGroup = "PropertyGroup";
-		private const string CSharp11 = "11.0";
-		private const string NetStandard21 = "netstandard2.1";
+		nameof(Vheos),
+	};
 
-		// Methods
-		private static string OnGeneratedCSProject(string _, string content)
+	// Const
+	private const string LangVersion = "LangVersion";
+	private const string Nullable = "Nullable";
+	private const string TargetFrameworkVersion = "TargetFrameworkVersion";
+	private const string TargetFramework = "TargetFramework";
+	private const string PropertyGroup = "PropertyGroup";
+	private const string CSharp11 = "11.0";
+	private const string NetStandard21 = "netstandard2.1";
+
+	// Methods
+	private static string ElementWithValue(string element, string value)
+		=> ElementOpen(element) + value + ElementClose(element);
+	private static string ElementOpen(string element)
+		=> $"<{element}>";
+	private static string ElementClose(string element)
+		=> $"</{element}>";
+
+	// Unity
+	private static string OnGeneratedCSProject(string path, string content)
+	{
+		var fileName = Path.GetFileNameWithoutExtension(path);
+		if (!Whitelist.Any(key => fileName.Contains(key)))
+			return content;
+
+		string pattern, replacement;
+		void Replace(int count = -1)
+			=> content = new Regex(pattern).Replace(content, replacement, count);
+
+		if (ForceCSharp11)
 		{
-			string pattern, replacement;
-			void Replace(int count = -1)
-				=> content = new Regex(pattern).Replace(content, replacement, count);
-
-#if FORCE_CSHARP11
 			// Recommended additional compiler argument: /langversion:preview
 			pattern = ElementWithValue(LangVersion, "(.*?)");
 			replacement = ElementWithValue(LangVersion, CSharp11);
 			Replace();
-#endif
+		}
 
-#if FORCE_NULLABLE
+		if (ForceNullable)
+		{
 			// Recommended additional compiler argument: /nullable:enable
 			pattern = $"(.*{ElementOpen(PropertyGroup)}.*)";
 			replacement = "$1\n" + ElementWithValue(Nullable, "enable");
 			Replace(1);
-#endif
+		}
 
-#if FORCE_NETSTANDARD21
+		if (ForceNetStandard21)
+		{
 			pattern = ElementWithValue(TargetFrameworkVersion, ".*?");
 			replacement = ElementWithValue(TargetFramework, NetStandard21);
 			Replace();
-#endif
-
-			return content;
 		}
 
-		private static string ElementWithValue(string element, string value)
-			=> ElementOpen(element) + value + ElementClose(element);
-		private static string ElementOpen(string element)
-			=> $"<{element}>";
-		private static string ElementClose(string element)
-			=> $"</{element}>";
+		return content;
 	}
 }
